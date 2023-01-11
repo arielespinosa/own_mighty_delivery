bcb34be52a13d5038cdfcb190ff52b278084fdda
DESKTOP-JB0FB4F\Ariel <arielpespinosa@gmail.com>
2023-01-11 14:47:15 +0100

    Modelos, Recursos y Controladores creados. Fichero commands ayuda a crear todo

---
+++ app/Http/Controllers/DeliveryManDocumentController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\DeliveryManDocument;
+use Illuminate\Http\Request;
+
+class DeliveryManDocumentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\DeliveryManDocument  $deliveryManDocument
+     * @return \Illuminate\Http\Response
+     */
+    public function show(DeliveryManDocument $deliveryManDocument)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\DeliveryManDocument  $deliveryManDocument
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(DeliveryManDocument $deliveryManDocument)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\DeliveryManDocument  $deliveryManDocument
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, DeliveryManDocument $deliveryManDocument)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\DeliveryManDocument  $deliveryManDocument
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(DeliveryManDocument $deliveryManDocument)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/OrdetHistoryController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\OrdetHistory;
+use Illuminate\Http\Request;
+
+class OrdetHistoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\OrdetHistory  $ordetHistory
+     * @return \Illuminate\Http\Response
+     */
+    public function show(OrdetHistory $ordetHistory)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\OrdetHistory  $ordetHistory
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(OrdetHistory $ordetHistory)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\OrdetHistory  $ordetHistory
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, OrdetHistory $ordetHistory)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\OrdetHistory  $ordetHistory
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(OrdetHistory $ordetHistory)
+    {
+        //
+    }
+}
